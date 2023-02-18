4ee2af26ecdbe5974de980df1078b445b6a91993
Qlbbl1111 <quinnlburke@icloud.com>
2023-02-18 12:41:36 -0800

    All controls work

--- src/subsytemFiles/flywheel.cpp
+++ src/subsytemFiles/flywheel.cpp
@@ -1,37 +1,41 @@
 #include "main.h"
 
-void setFlywheel(int a, int b) {
+bool latch = false;
+bool toggle = false;
+bool latch2 = false;
+bool toggle2 = false;
+int flywheelSpeed = 0;
+
+void setFlywheel(int a) {
     flywheel_a = a;
-    flywheel_b = b;
+    flywheel_b = a;
 }
 
 
 void setFlywheelMotors() {
-    bool latch = false;
-    bool toggle = false;
-    bool latch2 = false;
-    bool toggle2 = false;
-
-    if (toggle && !toggle2) //slow
-        setFlywheel(90, 90);
-    if (toggle2 && !toggle) //fast
-        setFlywheel(90, 90);
-    else flywheel = 0;
-
-    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && !shift()) {//slow
-        if  (!latch) {
+   //flywheel
+    if (toggle && !toggle2) { //slow
+        setFlywheel(100);
+    }
+    else if (toggle2 && !toggle) { //fast
+        setFlywheel(150);
+    }
+    else {
+        setFlywheel(0);
+    }
+    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && shift() == false) {//slow
+        if (!latch) {
             //flip the toggle one time and set the latch
             toggle = !toggle;
             latch = true;
         }
     }
-    else {
+    else{
         //Once the BumperA is released then then release the latch too
         latch = false;
     }
-
-    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && shift()) {//fast
-        if (!latch2){
+    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && shift() == true) {//fast
+        if (!latch2) {
             //flip the toggle one time and set the latch
             toggle2 = !toggle2;
             latch2 = true;
@@ -41,4 +45,5 @@ void setFlywheelMotors() {
         //Once the BumperA is released then then release the latch too
         latch2 = false;
     }
-}
+
+  }
